3f7d57d8007d9195454a4575d39cbf1d4566b186
Ko Zuke <kota.ishizuka1210@gmail.com>
2025-06-06 13:38:08 +0900

    Update configuration and App component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xl_board': '1500px',
+      },
+    },
   },
   plugins: [],
 };
